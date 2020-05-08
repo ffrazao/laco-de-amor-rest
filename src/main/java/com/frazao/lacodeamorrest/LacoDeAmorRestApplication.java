ec7a46bafce9edb2f6160e9331329181afb402d1
Fernando Frazão <ffrazao@gmail.com>
2020-05-08 10:50:24 -0300

    Reinicio
    Projeto reiniciado
    
    Signed-off-by: Fernando Frazão <ffrazao@gmail.com>

---
+++ src/main/java/com/frazao/lacodeamorrest/LacoDeAmorRestApplication.java
@@ -0,0 +1,13 @@
+package com.frazao.lacodeamorrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacoDeAmorRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacoDeAmorRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frazao/lacodeamorrest/LacoDeAmorRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frazao.lacodeamorrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacoDeAmorRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
